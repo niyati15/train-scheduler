bf5f9ca4a08d5c2330877a9781f86b6c57ffc744
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-05-09 16:47:08 -0700

    firebase details added

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// console.log("hello");
+var config = {
+    apiKey: "AIzaSyDWm9TMytWfvjceTHk3IyY1cPuYR5L_TyQ",
+    authDomain: "fir-b05cd.firebaseapp.com",
+    databaseURL: "https://fir-b05cd.firebaseio.com",
+    projectId: "fir-b05cd",
+    storageBucket: "fir-b05cd.appspot.com",
+    messagingSenderId: "203265362711"
+  };
+  firebase.initializeApp(config);
